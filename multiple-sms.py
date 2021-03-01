045f70ce8f9332ee3f25e4d68d8b084115a7c6bb
KenMwaura1 <kemwaura@gmail.com>
2021-03-01 15:04:52 +0300

    fixed issue with recipient getting multiple texts

--- multiple-sms.py
+++ multiple-sms.py
@@ -19,33 +19,20 @@ names_cell_range = sheet1['B2:B4']
 number_cell_range = sheet1['C2:C4']
 
 
-def number_generator(cell_range):
-    for number in number_cell_range:
-        for n in number:
-            yield n.value
-
-
-def name_generator():
-    for name in names_cell_range:
-        for n in name:
-            yield n.value
-
-
 def send_messages():
     client_name = ''
     recipients = ''
-    for each in name_generator():
-        client_name = each
-        for n in number_generator(number_cell_range):
-            recipients = str(f"+254{n}" )
-            print(recipients)
-
-            message = f"hey from python {client_name}"
-            try:
-                response = sms.send(message,[recipients,])
-                print(response)
-            except Exception as e:
-                print(f"Uh oh we have a problem: {e}")
+    for row in sheet1.iter_rows(values_only=True):
+        name = row[1]
+        number = f"+254{row[2]}"
+        print(name,number)
+        message = f"hey {name} from python using africas talking API"
+        try:
+            response = sms.send(message, [number])
+            print(response)
+        except Exception as e:
+            print(f"Uh oh we have a problem: {e}")
 
 
 send_messages()
+
