e2a1895a9e35a6722a9b866566c25a59291d2695
KenMwaura1 <kemwaura@gmail.com>
2021-03-01 12:21:31 +0300

    removed unnecessary folders

--- multiple-sms.py
+++ multiple-sms.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 username = os.getenv("username")
 api_key = os.getenv("api_key")
-
+at.initialize(username, api_key)
 sms = at.SMS
 
 at.initialize(username, api_key)
@@ -18,18 +18,34 @@ sheet1 = wb['Sheet1']
 names_cell_range = sheet1['B2:B4']
 number_cell_range = sheet1['C2:C4']
 
-for rows in names_cell_range:
-    for r in rows:
-        print(r.value)
-for rows in number_cell_range:
-    for r in rows:
-        print(r.value)
-def send_messages(self):
+
+def number_generator(cell_range):
+    for number in number_cell_range:
+        for n in number:
+            yield n.value
+
+
+def name_generator():
+    for name in names_cell_range:
+        for n in name:
+            yield n.value
+
+
+def send_messages():
+    client_name = ''
     recipients = ''
-    name = ''
-    message = f"hey from python {name}"
-    try:
-        response = self.sms.send(message, recipients, )
-        print(response)
-    except Exception as e:
-        print(f"Uh oh we have a problem: {e}")
+    for each in name_generator():
+        client_name = each
+        for n in number_generator(number_cell_range):
+            recipients = str(f"+254{n}" )
+            print(recipients)
+
+            message = f"hey from python {client_name}"
+            try:
+                response = sms.send(message,[recipients,])
+                print(response)
+            except Exception as e:
+                print(f"Uh oh we have a problem: {e}")
+
+
+send_messages()
