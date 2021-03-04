97d3d9c295fd1fd8a21a3bf10f0bcf3893c7222c
KenMwaura1 <kemwaura@gmail.com>
2021-03-04 12:44:54 +0300

    updated csv reader to parse each entry and send a customized message

--- multiple-sms-csv.py
+++ multiple-sms-csv.py
@@ -1,10 +1,37 @@
+import os
+import datetime
 import csv
 import africastalking as at
 from dotenv import load_dotenv
+# the load_dotenv function gets the environment variables defined in .env file
+load_dotenv()
+# assigns the variables to the environment variables
+api_key = os.getenv("api_key")
+username = os.getenv("username")
+# Initialize the Africas Talking client with the required credentials
+at.initialize(username, api_key)
+# assign the sms functionality to a variable
+sms = at.SMS
 
-with open('sample.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        name = row[1]
-        number = row[2]
-        print(name,number)
+
+def send_messages():
+    # parse the provided CSV with the inbuilt csv library
+    with open('sample.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            name = row[1]
+            number = row[2]
+            print(name, number)
+            # Get the current date and time
+            local_time = datetime.datetime.now()
+            # create a customized message
+            message = f"Hey{name}, this is a customized message sent on {local_time}"
+            # For each entry send a customized message
+            try:
+                response = sms.send(message, [number])
+                print(response)
+            except Exception as e:
+                print(f'Uh oh we have a problem: {e}')
+
+
+send_messages()
